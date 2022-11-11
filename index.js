27638b8a8ff27ef7a25a8d975c32630b0d3d5f23
NaxxSTR <naxo1005@gmail.com>
2022-11-11 18:46:12 -0300

    Conexion con base de datos

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ require('dotenv').config();
 const app = express();
 
 app.listen(process.env.PORT, () => {
-    console.log('Server Started')
+    console.log('El servidor corre en el puerto ->', process.env.PORT)
 });
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopoLogy', true);
+mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect(process.env.DB, (err) => {
     if (err) {
-        return console.log('Error al conectar con la base de datos ->', err)
+        return console.log('Error al conectar con la base de datos -> ', err)
     }
+    return console.log('Conectado a la base de datos')
 });
