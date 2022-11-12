956e430fcf51b5c9b38ec770160fb5d7cb1ab127
NaxxSTR <naxo1005@gmail.com>
2022-11-12 11:47:41 -0300

    arreglando main

--- routes/ApoderadoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const api = express.Router();
-const apoderadoController = require('../controllers/ApoderadoController');
-
-api.post('/apoderado', apoderadoController.createApoderado);
-api.get('/apoderados', apoderadoController.getApoderados);
-
-module.exports = api;
--- routes/AsistenteParvuloRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const api = express.Router();
-const AsistenteparvuloController = require('../controllers/AsistenteParvuloController');
-
-api.post('/Asistenteparvulo', AsistenteparvuloController.createAsistenteParvulo);
-api.get('/Asistenteparvulos', AsistenteparvuloController.getAsistenteParvulos);
-
-module.exports = api;
