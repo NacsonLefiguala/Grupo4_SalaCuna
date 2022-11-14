a6ac740200ff993a1a6a10d8dc19a9db69766057
NaxxSTR <naxo1005@gmail.com>
2022-11-13 23:12:46 -0300

    index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const parvuloRoutes = require('./routes/parvuloRoutes');
+const parvuloRoutes = require('./routes/ParvuloRoutes');
 
 app.use(cors())
 app.use(express.json());
