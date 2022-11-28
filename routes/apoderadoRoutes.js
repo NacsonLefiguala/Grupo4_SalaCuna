6858a114e60d05bdc0d4fd5048f9206be7b3d752
NaxxSTR <naxo1005@gmail.com>
2022-11-27 23:20:30 -0300

    midllewares

--- routes/apoderadoRoutes.js
+++ routes/apoderadoRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const api = express.Router();
 const customHeader = require('../middleware/customHeader')
 const ApoderadoController = require('../controllers/ApoderadoController');
-const ValidateCreate = require('../validators/ApoderadoValidators')
+const ValidatorCreate = require('../validators/ApoderadoValidators')
 
 
-api.post('/apoderado', ValidateCreate, customHeader(), ApoderadoController.createApoderado);
+api.post('/apoderado', ValidatorCreate, customHeader(), ApoderadoController.createApoderado);
 api.get('/apoderados', ApoderadoController.getApoderados);
 api.delete("/apoderado/:id", ApoderadoController.deleteApoderado);
 api.patch("/apoderado/:id", ApoderadoController.updateApoderado);
