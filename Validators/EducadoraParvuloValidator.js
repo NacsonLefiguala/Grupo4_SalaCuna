44dea5733d2fdb4d66f7cea3a74b29786797bb0e
NaxxSTR <naxo1005@gmail.com>
2022-11-28 22:29:20 -0300

    a

--- Validators/EducadoraParvuloValidator.js
+++ Validators/EducadoraParvuloValidator.js
@@ -1,5 +1,5 @@
 const { check, validationResult } = require('express-validator');
-const ValidateResult = require('../helpers/ValidateHelpers')
+const ValidateResult = require('../Helpers/ValidateHelpers')
 
 const ValidatorCreate = [
     check('NombreCompleto')
